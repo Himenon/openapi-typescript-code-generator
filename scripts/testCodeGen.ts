a6f303a0e9620e52e61f3e7e65e54780dcbf49b1
Himenon <k.himeno314@gmail.com>
2021-01-10 12:06:15 +0900

    ci: fix mkdir

--- scripts/testCodeGen.ts
+++ scripts/testCodeGen.ts
@@ -7,6 +7,7 @@ const main = () => {
     version: "v3",
     entryPoint: "test/api.test.domain/index.yml",
   };
+  fs.mkdirSync("test/code", { recursive: true });
   const code = CodeGenerator.generateTypeScriptCode(params);
   fs.writeFileSync("test/code/api.test.domain.ts", code, { encoding: "utf-8" });
   console.log(`Generate Code : test/code/api.test.domain.ts`);
