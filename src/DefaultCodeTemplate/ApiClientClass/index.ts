b61efa0521896fded45411c92baff3b3aa5c667f
Himenon <k.himeno314@gmail.com>
2021-03-23 02:02:17 +0900

    feat: Added option to output API Client in sync mode

--- src/DefaultCodeTemplate/ApiClientClass/index.ts
+++ src/DefaultCodeTemplate/ApiClientClass/index.ts
@@ -1,7 +1,7 @@
 import ts from "typescript";
 
 import { Factory } from "../../CodeGenerator";
-import { CodeGeneratorParams } from "../../Converter/v3";
+import type { CodeGenerator, CodeGeneratorParams } from "../../Converter/v3";
 import * as ApiClientInterface from "./ApiClientInterface";
 import * as Class from "./Class";
 import * as Constructor from "./Constructor";
@@ -9,10 +9,10 @@ import * as Method from "./Method";
 
 export { Method };
 
-export const create = (factory: Factory.Type, list: CodeGeneratorParams[]): ts.Statement[] => {
+export const create = (factory: Factory.Type, list: CodeGeneratorParams[], option: CodeGenerator.Option): ts.Statement[] => {
   const methodList = list.map(params => {
-    return Method.create(factory, params);
+    return Method.create(factory, params, option);
   });
   const members = [Constructor.create(factory), ...methodList];
-  return [...ApiClientInterface.create(factory, list), Class.create(factory, members)];
+  return [...ApiClientInterface.create(factory, list, option), Class.create(factory, members)];
 };
