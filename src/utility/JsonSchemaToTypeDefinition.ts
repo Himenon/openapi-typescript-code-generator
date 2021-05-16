b97b49e1307ea5b4020fca96ab59cd10f8f16bab
Himenon <k.himeno314@gmail.com>
2021-05-16 16:37:13 +0900

    chore: update

--- src/utility/JsonSchemaToTypeDefinition.ts
+++ src/utility/JsonSchemaToTypeDefinition.ts
@@ -53,9 +53,120 @@ export class Convert {
         value: [],
       });
     }
+    return this.generateTypeNode2(schema);
+  }
 
+  public static generateTypeNode2(schema: OpenApi.Schema | OpenApi.JSONSchema) {
+    switch (schema.type) {
+      case "boolean": {
+        const typeNode = factory.TypeNode.create({
+          type: "boolean",
+        });
+
+        return this.nullable(typeNode, !!schema.nullable);
+      }
+      case "null": {
+        return factory.TypeNode.create({
+          type: schema.type,
+        });
+      }
+      case "integer":
+      case "number": {
+        const items = schema.enum;
+        let typeNode: ts.TypeNode;
+        if (items && OpenApiType.Guard.isNumberArray(items)) {
+          typeNode = factory.TypeNode.create({
+            type: schema.type,
+            enum: items,
+          });
+        } else {
+          typeNode = factory.TypeNode.create({
+            type: schema.type,
+          });
+        }
+        return this.nullable(typeNode, !!schema.nullable);
+      }
+      case "string": {
+        const items = schema.enum;
+        let typeNode: ts.TypeNode;
+        if (items && OpenApiType.Guard.isStringArray(items)) {
+          typeNode = factory.TypeNode.create({
+            type: schema.type,
+            enum: items,
+          });
+        } else {
+          typeNode = factory.TypeNode.create({
+            type: schema.type,
+          });
+        }
+        return this.nullable(typeNode, !!schema.nullable);
+      }
+      case "array": {
+        if (Array.isArray(schema.items) || typeof schema.items === "boolean") {
+          throw new Error(`schema.items = ${JSON.stringify(schema.items)}`);
+        }
+        const typeNode = factory.TypeNode.create({
+          type: schema.type,
+          value: schema.items
+            ? this.generateTypeNode(schema.items)
+            : factory.TypeNode.create({
+                type: "undefined",
+              }),
+        });
+        return this.nullable(typeNode, !!schema.nullable);
+      }
+      case "object": {
+        const required: string[] = schema.required || [];
+        // // https://swagger.io/docs/specification/data-models/dictionaries/#free-form
+        if (schema.additionalProperties === true) {
+          return factory.TypeNode.create({
+            type: schema.type,
+            value: [],
+          });
+        }
+        const value: ts.PropertySignature[] = Object.entries(schema.properties || {}).map(([name, jsonSchema]) => {
+          return factory.PropertySignature.create({
+            name: name + "TODO",
+            type: this.generateTypeNode(jsonSchema),
+            optional: !required.includes(name),
+            comment: typeof jsonSchema !== "boolean" ? jsonSchema.description : undefined,
+          });
+        });
+        if (schema.additionalProperties) {
+          const additionalProperties = factory.IndexSignatureDeclaration.create({
+            name: "key",
+            type: this.generateTypeNode(schema.additionalProperties),
+          });
+          return factory.TypeNode.create({
+            type: schema.type,
+            value: [...value, additionalProperties],
+          });
+        }
+        const typeNode = factory.TypeNode.create({
+          type: schema.type,
+          value,
+        });
+        return this.nullable(typeNode, !!schema.nullable);
+      }
+      default:
+        break;
+    }
     return factory.TypeNode.create({
       type: "any",
     });
   }
+
+  public static nullable = (typeNode: ts.TypeNode, nullable: boolean): ts.TypeNode => {
+    if (nullable) {
+      return factory.UnionTypeNode.create({
+        typeNodes: [
+          typeNode,
+          factory.TypeNode.create({
+            type: "null",
+          }),
+        ],
+      });
+    }
+    return typeNode;
+  };
 }
