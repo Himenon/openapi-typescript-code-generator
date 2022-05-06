682af97f886d6fdc1ac46678cece4498549e5c82
K.Himeno <k.himeno314@gmail.com>
2022-05-06 20:02:03 +0900

    fix: remove require import (#80)

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,11 +1,25 @@
 import "./clean";
 
+import pkg from "../package.json";
+import * as fs from "fs";
+import { EOL } from "os";
 import { cherryPick } from "./tools/cherry-pick";
 import { copyPackageSet } from "./tools/copyPackageSet";
 import { generateExportsField } from "./tools/dualPackageSupport";
 import { shell } from "./tools/shell";
 
+const generateVersionTsFile = () => {
+  const codes: string[] = [`export const Name = "${pkg.name}";`, `export const Version = "${pkg.version}";`];
+  const tscCode = codes.join(EOL);
+  if (process.env.CI) {
+    console.log("`Update src/meta.ts file.");
+    fs.writeFileSync("src/meta.ts", tscCode, "utf-8");
+  }
+};
+
 const main = async () => {
+  generateVersionTsFile();
+
   await Promise.all([
     shell("pnpm tsc -p tsconfig.esm.json -d --emitDeclarationOnly --outDir ./lib/\\$types"),
     shell("pnpm tsc -p tsconfig.cjs.json"),
