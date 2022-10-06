a858a6065c2d7d0eea6727b3a5cfe051b2857756
K.Himeno <6715229+Himenon@users.noreply.github.com>
2022-10-06 16:50:24 +0900

    fix: file path

--- src/internal/OpenApiTools/components/Reference.ts
+++ src/internal/OpenApiTools/components/Reference.ts
@@ -1,4 +1,4 @@
-import * as path from "path";
+import { posix as path } from "path";
 
 import type { OpenApi } from "../../../types";
 import { DevelopmentError, FeatureDevelopmentError, NotFoundFileError } from "../../Exception";
