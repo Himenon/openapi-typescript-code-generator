7a7db58ac8cc216a9793b1bd32d33c7cb1b9ebb1
K.Himeno <6715229+Himenon@users.noreply.github.com>
2023-03-20 22:36:56 +0900

    chore: update

---
+++ test/__tests__/class/multi-type.test.domain.ts
@@ -0,0 +1,17 @@
+import * as fs from "fs";
+
+import * as Utils from "../../utils";
+
+describe("Multi Type", () => {
+  test("types", () => {
+    const generateCode = fs.readFileSync("test/code/class/mulit-type-test.domain/types.ts", { encoding: "utf-8" });
+    const text = Utils.replaceVersionInfo(generateCode);
+    expect(text).toMatchSnapshot();
+  });
+
+  test("apiClient", () => {
+    const generateCode = fs.readFileSync("test/code/class/mulit-type-test.domain/apiClient.ts", { encoding: "utf-8" });
+    const text = Utils.replaceVersionInfo(generateCode);
+    expect(text).toMatchSnapshot();
+  });
+});
---
+++ test/__tests__/functional/spit-code-test.ts
@@ -0,0 +1,17 @@
+import * as fs from "fs";
+
+import * as Utils from "../../utils";
+
+describe("Split Code", () => {
+  test("types", () => {
+    const generateCode = fs.readFileSync("test/code/functional/split/types.ts", { encoding: "utf-8" });
+    const text = Utils.replaceVersionInfo(generateCode);
+    expect(text).toMatchSnapshot();
+  });
+
+  test("apiClient", () => {
+    const generateCode = fs.readFileSync("test/code/functional/split/apiClient.ts", { encoding: "utf-8" });
+    const text = Utils.replaceVersionInfo(generateCode);
+    expect(text).toMatchSnapshot();
+  });
+});
