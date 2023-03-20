aeb317be6dfef2cb075e6d56e7d92cd430f87ead
K.Himeno <6715229+Himenon@users.noreply.github.com>
2023-03-21 00:03:13 +0900

    fix: export type Client for FunctionalApiClient (#101)

--- src/code-templates/functional-api-client/FunctionalApiClient/ClientTypeDefinition.ts
+++ src/code-templates/functional-api-client/FunctionalApiClient/ClientTypeDefinition.ts
@@ -1,11 +1,20 @@
+import ts from "typescript";
 import type { TsGenerator } from "../../../api";
 
-export const create = (factory: TsGenerator.Factory.Type) => {
-  return factory.TypeAliasDeclaration.create({
-    export: true,
-    name: "Client",
-    type: factory.TypeReferenceNode.create({
-      name: `ReturnType<typeof createClient>`,
+export const create = (factory: TsGenerator.Factory.Type): ts.TypeAliasDeclaration[] => {
+  return [
+    factory.TypeAliasDeclaration.create({
+      name: "ClientFunction<RequestOption>",
+      type: factory.TypeReferenceNode.create({
+        name: `typeof createClient<RequestOption>`,
+      })
+    }),
+    factory.TypeAliasDeclaration.create({
+      export: true,
+      name: "Client<RequestOption>",
+      type: factory.TypeReferenceNode.create({
+        name: `ReturnType<ClientFunction<RequestOption>>`,
+      })
     })
-  })
+  ]
 };
