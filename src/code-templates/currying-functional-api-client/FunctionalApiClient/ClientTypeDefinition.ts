78854889e55f4341dc471a0d0a1473188897ac46
K.Himeno <6715229+Himenon@users.noreply.github.com>
2023-04-01 22:51:49 +0900

    feat: add initialize

---
+++ src/code-templates/currying-functional-api-client/FunctionalApiClient/ClientTypeDefinition.ts
@@ -0,0 +1,20 @@
+import ts from "typescript";
+import type { TsGenerator } from "../../../api";
+
+export const create = (factory: TsGenerator.Factory.Type): ts.TypeAliasDeclaration[] => {
+  return [
+    factory.TypeAliasDeclaration.create({
+      name: "ClientFunction<RequestOption>",
+      type: factory.TypeReferenceNode.create({
+        name: `typeof createClient<RequestOption>`,
+      }),
+    }),
+    factory.TypeAliasDeclaration.create({
+      export: true,
+      name: "Client<RequestOption>",
+      type: factory.TypeReferenceNode.create({
+        name: `ReturnType<ClientFunction<RequestOption>>`,
+      }),
+    }),
+  ];
+};
