a7fdafce4bcb82d2fcb9bba23708f7d0e7bf63ec
K.Himeno <6715229+Himenon@users.noreply.github.com>
2023-06-23 21:27:06 +0900

    test: fux

--- src/code-templates/_shared/__tests__/utils.test.ts
+++ src/code-templates/_shared/__tests__/utils.test.ts
@@ -86,6 +86,6 @@ describe("Utils", () => {
 
   test("multiSplitStringToArray", () => {
     expect(Utils.multiSplitStringToArray("/{a}/b/{a}/c{a}/", ["{a}"])).toStrictEqual(["/", "{a}", "/b/", "{a}", "/c", "{a}", "/"]);
-    expect(Utils.multiSplitStringToArray("/a/b/{c}.json", ["{c}"])).toStrictEqual(["/a/b/", "{c}.json"]);
+    expect(Utils.multiSplitStringToArray("/a/b/{c}.json", ["{c}"])).toStrictEqual(["/a/b/", "{c}", ".json"]);
   })
 });
