bff8fdb2a9354925860cfed1dba2287c607c2286
K.Himeno <6715229+Himenon@users.noreply.github.com>
2024-02-01 21:57:27 +0900

    chore: update

--- scripts/tools/shell.ts
+++
@@ -1,9 +0,0 @@
-import execa from "execa";
-
-export const shell = (command: string, cwd: string = process.cwd()): execa.ExecaChildProcess<string> => {
-  return execa(command, {
-    stdio: ["pipe", "pipe", "inherit"],
-    shell: true,
-    cwd,
-  });
-};
