f5ca0277e24ae7befe21bbdc2765362f4159fb20
K.Himeno <6715229+Himenon@users.noreply.github.com>
2024-02-03 15:51:27 +0900

    chore: update

---
+++ examples/pure-js/gen.js
@@ -0,0 +1,11 @@
+import * as fs from "fs";
+
+import { CodeGenerator } from "@himenon/openapi-typescript-code-generator";
+
+const main = () => {
+  const codeGenerator = new CodeGenerator("spec/openapi.yml");
+  const code = codeGenerator.generateTypeDefinition();
+  fs.writeFileSync("client.ts", code, { encoding: "utf-8" });
+};
+
+main();
