6b92e8bf5a660371664904bc33191909be2b3572
K.Himeno <6715229+Himenon@users.noreply.github.com>
2024-02-03 16:17:37 +0900

    chore: tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
+  entry: ["./src/index.ts", "./src/api.ts", "./src/types.ts", "./src/templates.ts"],
+  outDir: "esm",
+  minify: false,
   target: "es2022",
   format: ["esm"],
   clean: true,
   dts: true,
+  tsconfig: "./tsconfig.build.json",
+  sourcemap: true,
 });
