ba1de3ec2d891d25ee61c0f6ed50a863431d586e
K.Himeno <6715229+Himenon@users.noreply.github.com>
2024-02-03 17:22:22 +0900

    chore: update

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,11 +3,10 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["./src/api.ts", "./src/index.ts", "./src/meta.ts", "./src/templates.ts", "./src/types.ts", "./src/utils.ts"],
   minify: false,
-  target: "es2020",
+  target: "es2022",
   format: ["cjs", "esm"],
   clean: true,
   dts: true,
-  splitting: false,
   tsconfig: "./tsconfig.build.json",
   sourcemap: true,
 });
