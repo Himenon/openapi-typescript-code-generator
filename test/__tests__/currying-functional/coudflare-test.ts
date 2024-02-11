9bee1eef9a64e32583ce14f4e947a74fe0021340
K.Himeno <6715229+Himenon@users.noreply.github.com>
2024-02-11 19:46:40 +0900

    chore: update snapshot

--- test/__tests__/currying-functional/coudflare-test.ts
+++ test/__tests__/currying-functional/coudflare-test.ts
@@ -4,7 +4,7 @@ import * as Utils from "../../utils";
 
 describe("Unknown", () => {
   test("client.ts", () => {
-    const generateCode = fs.readFileSync("test/code/currying-functional/cloudflare/code.ts", { encoding: "utf-8" });
+    const generateCode = fs.readFileSync("test/code/currying-functional/cloudflare/client.ts", { encoding: "utf-8" });
     const text = Utils.replaceVersionInfo(generateCode);
     expect(text).toMatchSnapshot();
   });
