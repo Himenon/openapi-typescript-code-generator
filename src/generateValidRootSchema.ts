3f39df08f942b2927033165cec8b6366415be28a
K.Himeno <6715229+Himenon@users.noreply.github.com>
2025-06-30 12:08:11 +0900

    fix: ref

--- src/generateValidRootSchema.ts
+++ src/generateValidRootSchema.ts
@@ -6,7 +6,24 @@ export const generateValidRootSchema = (input: Types.OpenApi.Document): Types.Op
   }
   /** update undefined operation id */
   for (const [path, methods] of Object.entries(input.paths || {})) {
-    for (const [method, operation] of Object.entries(methods || {})) {
+    const targets = {
+      get: methods.get,
+      put: methods.put,
+      post: methods.post,
+      delete: methods.delete,
+      options: methods.options,
+      head: methods.head,
+      patch: methods.patch,
+      trace: methods.trace,
+    } satisfies Record<string, Types.OpenApi.Operation | undefined>;
+    for (const [method, operation] of Object.entries(targets)) {
+      if (!operation) {
+        continue;
+      }
+      // skip reference object
+      if ("$ref" in operation) {
+        continue;
+      }
       if (!operation.operationId) {
         operation.operationId = `${method.toLowerCase()}${path.charAt(0).toUpperCase() + path.slice(1)}`;
       }
